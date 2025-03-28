2526398bad0abcb5dfcee168e6f621d222795337
Theo <71170619+Theo1337@users.noreply.github.com>
2025-03-28 14:17:05 -0300

    removed unfinished Discord feature

--- src/server.js
+++ src/server.js
@@ -6,73 +6,10 @@ const { translate } = require('bing-translate-api');
 
 const dev = process.env.NODE_ENV !== 'production';
 
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: ["DIRECT_MESSAGES", "GUILD_MESSAGES", "GUILDS", "GUILD_MEMBERS", "GUILD_PRESENCES", "GUILD_VOICE_STATES"], partials: ["CHANNEL"]});
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, StreamType, AudioPlayerStatus, VoiceConnectionStatus } = require("@discordjs/voice")
-
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
 let translation_language = "pt"
-const say = require('say')
-
-
-const player = createAudioPlayer();
-
-function playSong() {
-	const resource = createAudioResource(say.speak("Hello"), {
-		inputType: StreamType.Arbitrary,
-	});
-	player.play(resource);
-	return entersState(player, AudioPlayerStatus.Playing, 5000);
-}
-
-async function connectToChannel(channel) {
-	const connection = joinVoiceChannel({
-		channelId: channel.id,
-		guildId: channel.guild.id,
-		adapterCreator: channel.guild.voiceAdapterCreator,
-	});
-	try {
-		await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
-		return connection;
-	} catch (error) {
-		connection.destroy();
-		throw error;
-	}
-}
-
-
-client.on('ready', async () => {
-	console.log('Discord.js client is ready!');
-	try {
-		await playSong();
-		console.log('Song is ready to play!');
-	} catch (error) {
-		console.error(error);
-	}
-});
-
-client.on('messageCreate', async (message) => {
-	if (!message.guild) return;
-
-	if (message.content === '-join') {
-		const channel = message.member?.voice.channel;
-
-		if (channel) {
-			try {
-				const connection = await connectToChannel(channel);
-				connection.subscribe(player);
-				await message.reply('Playing now!');
-			} catch (error) {
-				console.error(error);
-			}
-		} else {
-			void message.reply('Join a voice channel then try again!');
-		}
-	}
-});
-
 
 io.on('connect', socket => {
     socket.on('room:join', roomName => {
@@ -112,5 +49,3 @@ nextApp.prepare().then(() => {
         console.log('[Server] Successfully started on port', process.env.PORT || 3000);
     });
 })
-
-client.login("MTA2MzQ3NTg0Njg1MTA2Nzk4NQ.GcYQor.31caebiuCjoldY52qRvljUlH_zD44zgU_2t-LM");
